214cd64e3e62f92da83aba774ade4e9065146ab3
James Onyekachi <okilonet1@gmail.com>
2022-07-06 20:08:54 +0100

    issue with photo loading

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,6 +1,7 @@
 import { GraphQLClient, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const token = process.env.GRAPHCMS_TOKEN;
 
 /** *************************************************************
  * Any file inside the folder pages/api is mapped to /api/* and  *
@@ -11,7 +12,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export default async function asynchandler(req, res) {
   const graphQLClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`,
+      authorization: `Bearer ${token}`,
     },
   });
 
