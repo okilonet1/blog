38663ae730c9a74d3b88aedab9649047f4e85fa8
James Onyekachi <okilonet1@gmail.com>
2022-07-06 01:46:26 +0100

    for now

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
 const PostCard = ({ post }) => {
   return (
-    <div>
-        {post.title}
-        {post.excerpt}
+    <div className='bg-white shadow-lg rounded-lg p=0 lg:p-8 pb-12 mb-8'>
+        <div className='relative overflow-hidden shadow-d pb-80 mb-6'>
+            {/* <img
+                src={post.feauturedImage.url}
+                alt={post.title}
+                className='object-top absolute h-80 w-full obejct-cocver shadow-lg rounded-t-lg lg:rounded-lg'           
+            /> */}
+        </div>
     </div>
   )
 }
