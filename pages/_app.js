2e6c2ab054c2b75b60b3ab7ef640631db877e4b6
James Onyekachi <okilonet1@gmail.com>
2022-07-06 19:06:04 +0100

    working on post display

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import React, {useEffect, useState} from 'react';
-import { Layout } from '../components';
+import React from "react";
 
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.scss';
+import "../styles/globals.scss";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
-)}
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
 
-export default MyApp
+export default MyApp;
